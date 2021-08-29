b0ae2a4cd9f611719ae9bbb26e8bc9199ee67ecb
TKShadowBlade <katanablack85@gmail.com>
2021-08-28 20:47:12 -0400

    local storage set update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
 var today = moment().format("dddd, MMM D, YYYY")
 currentDay.textContent = today
 
+$(document).ready(function() {
+    $(".saveBtn").click(function() {
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+
+    
+})
