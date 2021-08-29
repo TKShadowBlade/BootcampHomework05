281809af16f7bba61f624ce674ab4a62f207b4d6
TKShadowBlade <katanablack85@gmail.com>
2021-08-28 23:52:42 -0400

    updates for time blocks and local storage

--- assets/script.js
+++ assets/script.js
@@ -10,25 +10,39 @@ $(document).ready(function() {
 
     function getHours() {
         var currTime = moment().hour();
+        console.log(currTime);
 
         $(".time-block").each(function() {
-            var timeBlockEl = parseInt($(this).attr("id").split("hour")[1]);
+            var timeBlockEl = parseInt($(this).data("hour"));
 
             if(timeBlockEl < currTime) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            } else if (timeBlockEl === currTime) {
-                $(this).removeClass("future");
-                $(this).addClass("present");
-                $(this).removeClass("past");
-            } else {
-                $(this).addClass("future");
-                $(this).removeClass("present");
-                $(this).removeClass("past");
+                $(".time-block").removeClass("future");
+                $(".time-block").removeClass("present");
+                $(".time-block").addClass("past");
+            }
+            else if (timeBlockEl === currTime) {
+                $(".time-block").removeClass("future");
+                $(".time-block").removeClass("past");
+                $(".time-block").addClass("present");
+            }
+            else {
+                $(".time-block").removeClass("present");
+                $(".time-block").removeClass("past");
+                $(".time-block").addClass("future");
             }
             
         })
     }
+
+    $("#hr9 .description").val(localStorage.getItem("hr9"));
+    $("#hr10 .description").val(localStorage.getItem("hr10"));
+    $("#hr11 .description").val(localStorage.getItem("hr11"));
+    $("#hr12 .description").val(localStorage.getItem("hr12"));
+    $("#hr13 .description").val(localStorage.getItem("hr13"));
+    $("#hr14 .description").val(localStorage.getItem("hr14"));
+    $("#hr15 .description").val(localStorage.getItem("hr15"));
+    $("#hr16 .description").val(localStorage.getItem("hr16"));
+    $("#hr17 .description").val(localStorage.getItem("hr17"));
+
     getHours();
 })
